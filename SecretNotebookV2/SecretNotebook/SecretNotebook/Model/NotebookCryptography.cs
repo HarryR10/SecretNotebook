49f776275c62cb03054c5f7435e4666aa3e655df
igor_spirin <igor.r10@bk.ru>
2020-05-19 10:52:43 +0300

    -реализованы методы Encode/Decode

---
+++ SecretNotebookV2/SecretNotebook/SecretNotebook/Model/NotebookCryptography.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecretNotebook.Model
+{
+    public static class NotebookCryptography
+    {
+        public static byte[] Encode(byte[] data, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+
+            using (Aes crntAes = Aes.Create())
+            {
+                crntAes.Key = Key;
+                crntAes.IV = IV;
+
+                using (ICryptoTransform encryptor = crntAes.CreateEncryptor(crntAes.Key, crntAes.IV))
+                {
+                    encrypted = encryptor.TransformFinalBlock(data, 0, data.Length);
+                }
+            }
+
+            return encrypted;
+        }
+
+        public static byte[] Decode(byte[] data, byte[] Key, byte[] IV)
+        {
+            byte[] decrypted;
+
+            using (Aes crntAes = Aes.Create())
+            {
+                crntAes.Key = Key;
+                crntAes.IV = IV;
+
+                using (ICryptoTransform decryptor = crntAes.CreateDecryptor(crntAes.Key, crntAes.IV))
+                {
+                    decrypted = decryptor.TransformFinalBlock(data, 0, data.Length);
+                }
+            }
+
+            return decrypted;
+        }
+    }
+}
